5dbe4118b1e10fba85d1ecb97d7ffe77034230e9
dbartv <108138185+dbartv@users.noreply.github.com>
2025-06-19 13:02:17 +0200

    Cosmetics

--- PowerShellScripts/Build-Database.ps1
+++ PowerShellScripts/Build-Database.ps1
@@ -19,14 +19,13 @@ $StartTime = Get-Date
 #==================================================================================================
 #Drop and create output database.
 #==================================================================================================
-$Query ="
-IF EXISTS (SELECT * FROM sys.databases WHERE [name] = '$DbName')
-BEGIN
-  ALTER DATABASE [$DbName] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
-  DROP DATABASE  [$DbName];
-END;
-CREATE DATABASE [$DbName];
-ALTER DATABASE  [$DbName] SET RECOVERY SIMPLE WITH NO_WAIT;"
+$Query ="IF EXISTS (SELECT * FROM sys.databases WHERE [name] = '$DbName')
+          BEGIN
+            ALTER DATABASE [$DbName] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+            DROP DATABASE  [$DbName];
+          END;
+          CREATE DATABASE [$DbName];
+          ALTER DATABASE  [$DbName] SET RECOVERY SIMPLE WITH NO_WAIT;"
 try 
 {
   Invoke-SqlCmd -TrustServerCertificate -ServerInstance $ServerName -Database master -Query $Query -ErrorAction Stop
